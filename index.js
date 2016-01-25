1bddcf31dfcb9b90e88b9c0464f2ba442e504984
M.C. van den Boogaart <scola@kvdb.name>
2016-01-25 15:12:34 +0100

    Initial import

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  Collection: require('./lib/collection.js'),
+  Item: require('./lib/item.js'),
+  List: require('./lib/list.js'),
+  Messenger: require('./lib/messenger.js')
+};
