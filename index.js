316e17ecf4be3cb7abb15ac71804201ad84482e2
M.C. van den Boogaart <scola@kvdb.name>
2016-02-01 16:09:05 +0100

    Use dispatcher, refactor

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const DI = require('@scola/di');
+
+const Dispatcher = require('./lib/dispatcher');
+const Messenger = require('./lib/messenger');
+const Model = require('./lib/model');
+
+class Module extends DI.Module {
+  configure() {
+    this.inject(Messenger).with(
+      this.singleton(Dispatcher)
+    );
+  }
+}
+
 module.exports = {
-  Messenger: require('./lib/messenger'),
-  Model: require('./lib/model')
+  Dispatcher,
+  Messenger,
+  Model,
+  Module
 };
